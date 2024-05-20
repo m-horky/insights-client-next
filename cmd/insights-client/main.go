fe497cafa7d3e4dee82ad06ba80f0bc78271c1b4
mhorky <mhorky@redhat.com>
2024-05-20 13:11:23 +0200

    chore: Use urfave/cli/v3

--- cmd/insights-client/main.go
+++ cmd/insights-client/main.go
@@ -1,18 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/m-horky/insights-client-next/internal/api/inventory"
 	"github.com/m-horky/insights-client-next/internal/system"
+	"github.com/urfave/cli/v3"
 	"log/slog"
 	"os"
-	"strings"
-
-	"github.com/urfave/cli/v2"
 
 	"github.com/m-horky/insights-client-next/internal/configuration"
 	"github.com/m-horky/insights-client-next/internal/constants"
-	"github.com/m-horky/insights-client-next/internal/core"
 	"github.com/m-horky/insights-client-next/internal/enums"
 )
 
@@ -21,108 +19,75 @@ func init() {
 	slog.SetDefault(slog.New(slog.NewTextHandler(
 		os.Stderr, &slog.HandlerOptions{Level: config.LogLevel},
 	)))
+
+	cli.HelpFlag = &cli.BoolFlag{Name: "help"}
+
+	cli.VersionFlag = &cli.BoolFlag{Name: "version"}
+	cli.VersionPrinter = func(cmd *cli.Command) {
+		fmt.Printf("Insights Client: %s\n", constants.Version)
+		fmt.Printf("Insights Core:   none\n")
+	}
 }
 
 func main() {
-	app := cli.NewApp()
-	app.HideHelpCommand = true
-	app.HideHelp = true
-	app.HideVersion = true
-	app.Action = run
-	app.CustomAppHelpTemplate = getHelp()
-
-	app.Name = "insights-client"
-	app.Usage = "Manage Red Hat Insights."
-	app.Version = constants.Version
-
-	app.Flags = []cli.Flag{
-		// modifiers
-		&cli.StringFlag{
-			Name:  "format",
-			Value: "human",
-			Action: func(context *cli.Context, format string) error {
-				if _, err := enums.ParseFormat(format); err != nil {
-					return err
-				}
-				return nil
+	cmd := &cli.Command{
+		Name:            "insights-client",
+		HideHelpCommand: true,
+		Version:         constants.Version,
+		Usage:           "Manage data connection for Red Hat Insights",
+		UsageText:       fmt.Sprintf("%s COMMAND [FLAGS...]", os.Args[0]),
+		Flags: []cli.Flag{
+			&cli.BoolFlag{Name: "register", Category: "host", Usage: "register the host"},
+			&cli.BoolFlag{Name: "unregister", Category: "host", Usage: "unregister the host"},
+			&cli.BoolFlag{Name: "status", Category: "host", Usage: "display host status"},
+			&cli.BoolFlag{Name: "display-name", Category: "host", Usage: "set host display name in Inventory"},
+			&cli.BoolFlag{Name: "ansible-host", Category: "host", Usage: "set Ansible hostname in Inventory"},
+			&cli.StringFlag{Name: "collector", Category: "data collection", Usage: "run collector"},
+			// Deprecated commands
+			&cli.BoolFlag{Name: "test-connection", Category: "deprecated", Usage: "alias for '--status'"},
+			&cli.BoolFlag{Name: "compliance", Category: "deprecated", Usage: "alias for '--collector compliance'"},
+			// Flags
+			&cli.StringFlag{
+				Name:     "format",
+				Category: "global flags",
+				Value:    "human",
+				Action: func(ctx context.Context, cmd *cli.Command, s string) error {
+					if s != "human" && s != "json" {
+						fmt.Printf("Error: invalid format: '%s'\n", s)
+						return fmt.Errorf("invalid format: %s", s)
+					}
+					return nil
+				},
 			},
 		},
-		// commands
-		&cli.BoolFlag{Name: "register"},
-		&cli.BoolFlag{Name: "unregister"},
-		&cli.StringFlag{Name: "display-name"},
-		&cli.StringFlag{Name: "ansible-host"},
-		&cli.BoolFlag{Name: "version"},
-		&cli.BoolFlag{Name: "status"},
-		&cli.BoolFlag{Name: "help"},
-		&cli.StringFlag{
-			Name: "collector", Value: "advisor",
-			Action: func(context *cli.Context, collector string) error {
-				return core.VerifyCollector(collector)
-			},
-		},
-		// TODO --collector-list?
-		// deprecated commands
-		&cli.BoolFlag{Name: "test-connection"},
+		Action: run,
 	}
 
-	if err := app.Run(os.Args); err != nil {
-		slog.Error("could not run insights-client", slog.Any("error", err))
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		slog.Error("crashed", slog.Any("error", err))
+		os.Exit(1)
 	}
-}
-
-// getHelp creates custom help output
-// because urfave/cli/v2 doesn't seem to be flexible enough to not support flag-only setup.
-func getHelp() string {
-	builder := strings.Builder{}
-
-	builder.WriteString("Usage: insights-client COMMAND [FLAGS...]\n")
-	builder.WriteString("\n")
-	builder.WriteString("Manage data collection for Red Hat Insights.\n")
-
-	// We can lie a bit; we can pretend --display-name is only available during registration.
-	builder.WriteString("\nCOMMANDS\n")
-	builder.WriteString("--register               register the system\n")
-	builder.WriteString("  [--display-name NAME]    and set Insights hostname\n")
-	builder.WriteString("  [--ansible-host NAME]    and set Ansible hostname\n")
-	builder.WriteString("--unregister             unregister the host\n")
-	builder.WriteString("--status                 display system status\n")
-	builder.WriteString("--collector APP [...]    run collector\n")
-	builder.WriteString("--version                display program version\n")
-	builder.WriteString("--help                   display this help\n")
-
-	builder.WriteString("\nDEPRECATED COMMANDS\n")
-	builder.WriteString("--test-connection        use --status\n")
-	builder.WriteString("--compliance [...]       use --collector compliance [...]\n")
-
-	builder.WriteString("\nFLAGS\n")
-	builder.WriteString("--format FORMAT          output format (options: human, json)\n")
-
-	return builder.String()
 }
 
 // run acts as an action router.
-func run(c *cli.Context) error {
+func run(ctx context.Context, cmd *cli.Command) error {
+	cli.DefaultAppComplete(ctx, cmd)
+
 	// Flags
-	if _, err := enums.ParseFormat(c.String("format")); err != nil {
+	if _, err := enums.ParseFormat(cmd.String("format")); err != nil {
 		slog.Error("could not parse format", slog.Any("error", err))
 		return err
 	}
 
 	// Deprecated commands
 	for _, flag := range []string{"test-connection"} {
-		if c.IsSet(flag) {
+		if cmd.IsSet(flag) {
 			slog.Warn("flag is deprecated", slog.String("flag", flag))
 		}
 	}
 
 	// Commands
-	if c.Bool("version") {
-		fmt.Printf("Insights Client: %s\n", constants.Version)
-		fmt.Printf("Insights Core:   none\n")
-		return nil
-	}
-	if c.Bool("register") {
+	if cmd.Bool("register") {
 		if _, err := system.GetInventoryHost(); err == nil {
 			fmt.Printf("Error: This host is already registered.\n")
 			return nil
@@ -130,31 +95,32 @@ func run(c *cli.Context) error {
 		slog.Warn("register: not implemented")
 		return nil
 	}
-	if c.Bool("unregister") {
+	if cmd.Bool("unregister") {
 		slog.Warn("unregister: not implemented")
 		return nil
 	}
-	if c.IsSet("display-name") {
+	if cmd.IsSet("display-name") {
 		host, err := system.GetInventoryHost()
 		if err != nil {
 			fmt.Println("Error: Host is not registered.")
 			return nil
 		}
-		err = inventory.UpdateDisplayName(host.InsightsInventoryID, c.String("display-name"))
+		err = inventory.UpdateDisplayName(host.InsightsInventoryID, cmd.String("display-name"))
 		if err != nil {
 			return err
 		}
 		fmt.Println("OK: Display name has been updated.")
 	}
-	if c.Bool("status") || c.Bool("test-connection") {
+	if cmd.Bool("status") || cmd.Bool("test-connection") {
 		slog.Warn("status: not implemented")
 		return nil
 	}
-	if c.IsSet("collector") {
+	if cmd.IsSet("collector") {
 		slog.Warn("collector: not implemented")
 		return nil
 	}
 
 	// FIXME Implicitly we should run Advisor collection instead.
+	slog.Warn("default: not implemented")
 	return nil
 }
--- go.mod
+++ go.mod
@@ -4,15 +4,13 @@ go 1.22.2
 
 require (
 	github.com/gookit/ini/v2 v2.2.3
-	github.com/urfave/cli/v2 v2.27.2
+	github.com/urfave/cli/v3 v3.0.0-alpha9
 )
 
 require (
-	github.com/cpuguy83/go-md2man/v2 v2.0.4 // indirect
 	github.com/gookit/color v1.5.4 // indirect
 	github.com/gookit/goutil v0.6.15 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
-	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	github.com/xrash/smetrics v0.0.0-20240312152122-5f08fbb34913 // indirect
 	golang.org/x/sync v0.5.0 // indirect
