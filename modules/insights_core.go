5c690f855fa6e4684c02a75ef82203bfbddf25b1
mhorky <mhorky@redhat.com>
2024-09-23 17:06:14 +0200

    feat: Display module version if the module is part of Core

--- modules/insights_core.go
+++ modules/insights_core.go
@@ -1,14 +1,18 @@
 package modules
 
 import (
+	"bytes"
 	"fmt"
+	"log/slog"
 	"os"
+	"os/exec"
+	"strings"
 )
 
 func GetAdvisorModule() *Module {
 	return &Module{
 		Name:    "advisor",
-		Version: "??? (not implemented)",
+		Version: getInsightsCoreVersion(),
 		Env:     getInsightsCoreEnv(),
 		Exec:    []string{"python3", "-m", "insights.client.phase.v2"},
 		Commands: [][]string{
@@ -27,7 +31,7 @@ func GetAdvisorModule() *Module {
 func GetComplianceModule() *Module {
 	return &Module{
 		Name:    "compliance",
-		Version: "??? (not implemented)",
+		Version: getInsightsCoreVersion(),
 		Env:     getInsightsCoreEnv(),
 		Exec:    []string{"python3", "-m", "insights.client.phase.v2"},
 		Commands: [][]string{
@@ -41,7 +45,7 @@ func GetComplianceModule() *Module {
 func GetMalwareModule() *Module {
 	return &Module{
 		Name:    "malware",
-		Version: "??? (not implemented)",
+		Version: getInsightsCoreVersion(),
 		Env:     getInsightsCoreEnv(),
 		Exec:    []string{"python3", "-m", "insights.client.phase.v2"},
 		Commands: [][]string{
@@ -66,4 +70,44 @@ func getInsightsCoreEnv() []string {
 		env = append(env, fmt.Sprintf("PYTHONPATH=%s", InsightsCorePath+":"+pythonPath))
 	}
 	return env
+}
+
+var insightsCoreVersionIsCached = false
+var insightsCoreVersion = ""
+
+// getInsightsCoreVersion runs the Core to figure out what version it has.
+//
+// Since the Core does not have its metadata in a file, we need to read this dynamically.
+func getInsightsCoreVersion() string {
+	if insightsCoreVersionIsCached {
+		return insightsCoreVersion
+	}
+
+	cmd := exec.Command("python3", "-c", "from insights.client import InsightsClient; print(InsightsClient(None, False).version())")
+	for _, variable := range os.Environ() {
+		cmd.Env = append(cmd.Env, variable)
+	}
+	cmd.Env = append(cmd.Env, fmt.Sprintf(`PYTHONPATH=/etc/insights-client/rpm.egg:%s"`, os.Getenv(`PYTHONPATH`)))
+
+	var stdoutBuffer, stderrBuffer bytes.Buffer
+	cmd.Stdout = &stdoutBuffer
+	cmd.Stderr = &stderrBuffer
+
+	err := cmd.Run()
+
+	if err != nil {
+		slog.Error(
+			"could not request Core version",
+			slog.String("error", err.Error()),
+			slog.Any("stdout", stdoutBuffer.String()),
+			slog.Any("stderr", stderrBuffer.String()),
+		)
+		insightsCoreVersion = "??? (parsing error)"
+		insightsCoreVersionIsCached = true
+		return insightsCoreVersion
+	}
+
+	insightsCoreVersion = strings.TrimSpace(stdoutBuffer.String())
+	insightsCoreVersionIsCached = true
+	return insightsCoreVersion
 }
