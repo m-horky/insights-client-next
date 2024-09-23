54ab411023f36a91be1e7e3115f9c95c255a9821
mhorky <mhorky@redhat.com>
2024-09-23 17:06:28 +0200

    feat: Vertically align module versions for --module-list

--- internal/impl/actions_module.go
+++ internal/impl/actions_module.go
@@ -2,6 +2,7 @@ package impl
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/m-horky/insights-client-next/internal"
 	"github.com/m-horky/insights-client-next/modules"
@@ -9,8 +10,16 @@ import (
 
 func RunListModules(input *Input) internal.IError {
 	fmt.Println("Available modules:")
+
+	maxLength := 0
 	for _, module := range modules.GetModules() {
-		fmt.Printf("* %s %s\n", module.Name, module.Version)
+		if len(module.Name) > maxLength {
+			maxLength = len(module.Name)
+		}
+	}
+
+	for _, module := range modules.GetModules() {
+		fmt.Printf("* %s  %s\n", module.Name+strings.Repeat(" ", maxLength-len(module.Name)), module.Version)
 	}
 	return nil
 }
